7414a0fe0f9e6ae35a459a9bde61a3910929feea
yncute <dionedajalen@gmail.com>
2024-02-01 18:34:15 -0800

    side menu and navigation to different screens implemented

--- App.js
+++ App.js
@@ -1,12 +1,68 @@
+import React, { useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, ScrollView, TextInput, Image } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+import Homepage from './screens/Homepage'
+import ProfileScreen from './screens/Profile';
+import SettingsScreen from './screens/Settings';
+import SavedScreen from './screens/Saved';
+import ReferScreen from './screens/Refer';
+import DrawerItems from './constants/DrawerItems';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator
+        drawerType="front"
+        initialRouteName="Profile"
+        drawerContentOptions={{
+          activeTintColor: '#e91e63',
+          itemStyle: {marginVertical: 10},
+        }}
+      >
+        {
+          DrawerItems.map(drawer=>
+          <Drawer.Screen
+            key={drawer.name}
+            name={drawer.name}
+            options={{
+              drawerIcon:({focused})=>
+              drawer.iconType==='Material' ?
+                <MaterialCommunityIcons
+                name={drawer.iconName}
+                />
+              :
+              drawer.iconType==='Feather' ?
+                <Feather
+                  name={drawer.iconName}
+                  size={24}
+                  color={focused ? "#e91e63" : "black"}
+                />
+              :
+              <FontAwesome5
+                name={drawer.iconName}
+                size={24}
+                colo={focused ? "#e91e63" : "black"}
+              />
+            }}
+            component={
+              drawer.name==='Profile' ? ProfileScreen
+             : drawer.name==='Settings' ? SettingsScreen
+               : drawer.name==='Saved Items' ? SavedScreen
+                 : ReferScreen
+            }
+          />
+          )
+        }
+    </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
