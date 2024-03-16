fe51b1190a96f8e6384c64d9812cac27792dfc56
David Ryan <42628566+davidry777@users.noreply.github.com>
2024-03-15 17:47:04 -0700

    this is actually the adjusting pic per percentage thingy

--- src/components/AvatarUtils.js
+++ src/components/AvatarUtils.js
@@ -12,7 +12,30 @@ import fatcat4 from "../assets/Cats/fatcat4.jpg";
 const generateAvatar = (user) => {
   const avatarImages = [thincat1, thincat2, thincat3, thincat4];
   const fatCatImages = [fatcat1, fatcat2, fatcat3, fatcat4];
-  const isFatCat = user.macros > 50;
+  const proteinGoal = user.protein >= user.proteinGoal;
+  const fatGoal = (user.fat >= user.fatGoal)? 1:0;
+  const carbGoal = (user.carbohydrate >= user.carbGoal) ? 1:0;
+  const calorieGoal = (user.calories >= user.calorieGoal)? 1:0;
+  const totalGoal = proteinGoal + fatGoal + carbGoal + calorieGoal;
+  var _avatar = fatcat1;
+  if (totalGoal ==3 ) {  
+    _avatar = fatcat3;
+  }
+  else if (totalGoal ==2 ) {
+    _avatar = fatcat2;
+  }
+  else if (totalGoal ==2 ) {
+    _avatar = fatcat2;
+  }
+  else if (totalGoal ==1 ) {
+    _avatar = thincat2;
+  }
+  else if (totalGoal ==1 ) {
+    _avatar = thincat1;
+  }
+  const isFatCat = user.calories + user.protein + user.fat + user.carbohydrate > 50;
+  console.log(user);
+ 
 
   const randomIndex = 1; //Math.floor(Math.random() * 4);
   //console.log("Avatar Images:", avatarImages);
